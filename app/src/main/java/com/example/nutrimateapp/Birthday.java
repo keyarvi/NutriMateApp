4f208ea73abef3093fa6ba34e6cc7af27502ff1b
krvt <tabiquerokhristopher1@gmail.com>
2025-06-24 21:02:03 +0800

    back and continue buttons of name sex birthday

--- app/src/main/java/com/example/nutrimateapp/Birthday.java
+++ app/src/main/java/com/example/nutrimateapp/Birthday.java
@@ -1,6 +1,9 @@
 package com.example.nutrimateapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,32 @@ public class Birthday extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_birthday);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Back button to go to Name activity
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Birthday.this, Name.class);
+                startActivity(intent);
+                finish(); // optional to prevent stacking
+            }
+        });
+
+        // Continue button to go to Sex activity
+        Button continueButton = findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Birthday.this, Sex.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/nutrimateapp/Sex.java
+++ app/src/main/java/com/example/nutrimateapp/Sex.java
@@ -1,6 +1,9 @@
 package com.example.nutrimateapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,22 @@ public class Sex extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sex);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Back button to go to Birthday activity
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Sex.this, Birthday.class);
+                startActivity(intent);
+                finish(); // Optional to avoid stacking activities
+            }
         });
     }
 }
